775498076ce6221856db0f206f697740d9859ad2
DarwinDemian <darwindemian@hotmail.com>
2022-03-01 07:06:52 -0300

    Updated Matrix.js

--- Matrix.js
+++ Matrix.js
@@ -1,14 +1,6 @@
-// YES, this code is kind of jank and I felt the need to write a bit too much documentation, but as a great man would put it:
+// YES, this code is kind of jank but as a great man would put it:
 // “It’s not a tumor! It’s not a tumor, at all.”
 // - Arnold Schwarzenegger, Kindergarten Cop
-
-// FUNCTION CALL RUNDOWN
-// isBorder calls checkAdjacent if there's an adjacent 1 in a border
-// checkAdjacent calls moveRow which first calls testAdjacent to check 1s in other directions and then moveRow can proceed to check its current row
-// testAdjacent calls the necessary move functions and the process repeats until there's no more adjacent 1s
-// in the end, a nested for loop goes through the matrix converting every 1 not found in the adjacent object
-
-// Refer to the diagram for more clearity
 
 const removeIslands = (matrix) => {
   // Object to hold all adjacent 1s positions
@@ -23,124 +15,8 @@ const removeIslands = (matrix) => {
     adjacent[col] = [];
   }
 
-  // Starting from each 1 found in a border
-  // Check/move through the array looking for adjacents 1s to that border
-  const checkAdjacent = (col, row, matrix, direction) => {  
-    
-    // Check for adjacents and call the necessary move function
-    const testAdjacent = (col, row, matrix, direction) => {
-      if (direction === "right") {
-        if (
-          matrix[col][row + 1] === 1 &&
-          adjacent[col].indexOf(row + 1) in adjacent[col] === false
-        ) {
-          adjacent[col].push(row + 1);
-          moveRow(col, row + 1, matrix, "right");
-        }
-      }
-
-      if (direction === "left") {
-        if (
-          matrix[col][row - 1] === 1 &&
-          adjacent[col].indexOf(row - 1) in adjacent[col] === false
-        ) {
-          adjacent[col].push(row - 1);
-          moveRow(col, row - 1, matrix, "left");
-        }
-      }
-
-      if (direction === "top") {
-        if (
-          adjacent[col - 1] !== undefined &&
-          matrix[col - 1][row] === 1 &&
-          adjacent[col - 1].indexOf(row) in adjacent[col] === false
-        ) {
-          adjacent[col - 1].push(row);
-          moveTopBot(col - 1, row, matrix, "top");
-        }
-      }
-
-      if (direction === "bottom") {
-        if (
-          adjacent[col + 1] !== undefined &&
-          matrix[col + 1][row] === 1 &&
-          adjacent[col + 1].indexOf(row) in adjacent[col] === false
-        ) {
-          adjacent[col + 1].push(row);
-          moveTopBot(col + 1, row, matrix, "bottom");
-        }
-      }
-    };
-
-    // Call testAdjacent to check for adjacent 1s in other directions 
-    // move through the matrix in the necessary direction
-    // until you've reached the end of the column/row or
-    // there's no more adjacent 1s
-    const moveTopBot = (col, row, matrix, direction) => {
-      if (direction === "top") {
-        for (col; colLength > col; col--) {
-          testAdjacent(col, row, matrix, "left");
-          testAdjacent(col, row, matrix, "right");
-
-          if (
-            matrix[col - 1] !== undefined &&
-            matrix[col - 1][row] === 1 &&
-            adjacent[col - 1].indexOf(row) in adjacent[col] === false
-          ) {
-            adjacent[col - 1].push(row);
-          } else break;
-        }
-      } else if (direction === "bottom") {
-        for (col; col < colLength; col++) {
-          testAdjacent(col, row, matrix, "left");
-          testAdjacent(col, row, matrix, "right");
-
-          if (
-            matrix[col + 1] !== undefined &&
-            matrix[col + 1][row] === 1 &&
-            adjacent[col + 1].indexOf(row) in adjacent[col] === false
-          ) {
-            adjacent[col + 1].push(row);
-          } else break;
-        }
-      }
-    };
-
-    const moveRow = (col, row, matrix, direction) => {
-      if (direction === "right") {
-        for (row; row < rowLength - 2; row++) {
-          testAdjacent(col, row, matrix, "top");
-          testAdjacent(col, row, matrix, "bottom");
-
-          if (
-            matrix[col][row + 1] === 1 &&
-            adjacent[col].indexOf(row + 1) in adjacent[col] === false
-          ) {
-            adjacent[col].push(row + 1);
-          } else break;
-        }
-      } else if (direction === "left") {
-        for (row; row > 1; row--) {
-          testAdjacent(col, row, matrix, "top");
-          testAdjacent(col, row, matrix, "bottom");
-
-          if (
-            matrix[col][row - 1] === 1 &&
-            adjacent[col].indexOf(row - 1) in adjacent[col] === false
-          ) {
-            adjacent[col].push(row - 1);
-          } else break;
-        }
-      }
-    };
-
-    // Start the search
-    const initialize = () => moveRow(col, row, matrix, direction);
-    initialize();
-  };
-
   // Check every border
-  const isBorder = (matrix) => {
+  const isBorder = () => {
     for (let col = 0; col < colLength; col++) {
       // Check for leftBorder
       if (matrix[col][0] === 1) {
@@ -150,7 +26,7 @@ const removeIslands = (matrix) => {
           adjacent[col].indexOf(1) in adjacent[col] === false
         ) {
           adjacent[col].push(1);
-          checkAdjacent(col, 1, matrix, "right");
+          moveRow(col, 1, "right");
         }
       }
 
@@ -162,19 +38,125 @@ const removeIslands = (matrix) => {
           adjacent[col].indexOf(rowLength - 2) in adjacent[col] === false
         ) {
           adjacent[col].push(rowLength - 2);
-          checkAdjacent(col, rowLength - 2, matrix, "left");
+          moveRow(col, rowLength - 2, "left");
         }
       }
     }
   };
 
+  // move through the matrix in the direction
+  // until you've reached the end of the column/row or
+  // there's no more adjacent 1s
+  const moveRow = (col, row, direction) => {
+    if (direction === "right") {
+      for (row; row < rowLength; row++) {
+        checkAdjacent(col, row, "top");
+        checkAdjacent(col, row, "bottom");
+
+        if (
+          matrix[col][row + 1] === 1 &&
+          adjacent[col].indexOf(row + 1) in adjacent[col] === false
+        ) {
+          adjacent[col].push(row + 1);
+        } else break;
+      }
+    } else if (direction === "left") {
+      for (row; row > 0; row--) {
+        checkAdjacent(col, row, "top");
+        checkAdjacent(col, row, "bottom");
+
+        if (
+          matrix[col][row - 1] === 1 &&
+          adjacent[col].indexOf(row - 1) in adjacent[col] === false
+        ) {
+          adjacent[col].push(row - 1);
+        } else break;
+      }
+    }
+  };
+
+
+  const moveTopBot = (col, row, direction) => {
+    if (direction === "top") {
+      for (col; col > -1; col--) {
+        checkAdjacent(col, row, "left");
+        checkAdjacent(col, row, "right");
+
+        if (
+          matrix[col - 1] !== undefined &&
+          matrix[col - 1][row] === 1 &&
+          adjacent[col - 1].indexOf(row) in adjacent[col] === false
+        ) {
+          adjacent[col - 1].push(row);
+        } else break;
+      }
+    } else if (direction === "bottom") {
+      for (col; col < colLength; col++) {
+        checkAdjacent(col, row, "left");
+        checkAdjacent(col, row, "right");
+
+        if (
+          matrix[col + 1] !== undefined &&
+          matrix[col + 1][row] === 1 &&
+          adjacent[col + 1].indexOf(row) in adjacent[col] === false
+        ) {
+          adjacent[col + 1].push(row);
+        } else break;
+      }
+    }
+  };
+
+  // Check for adjacents and call the necessary move function
+  const checkAdjacent = (col, row, direction) => {
+    if (direction === "right") {
+      if (
+        matrix[col][row + 1] === 1 &&
+        adjacent[col].indexOf(row + 1) in adjacent[col] === false
+      ) {
+        adjacent[col].push(row + 1);
+        moveRow(col, row + 1, "right");
+      }
+    }
+
+    if (direction === "left") {
+      if (
+        matrix[col][row - 1] === 1 &&
+        adjacent[col].indexOf(row - 1) in adjacent[col] === false
+      ) {
+        adjacent[col].push(row - 1);
+        moveRow(col, row - 1, "left");
+      }
+    }
+
+    if (direction === "top") {
+      if (
+        adjacent[col - 1] !== undefined &&
+        matrix[col - 1][row] === 1 &&
+        adjacent[col - 1].indexOf(row) in adjacent[col] === false
+      ) {
+        adjacent[col - 1].push(row);
+        moveTopBot(col - 1, row, "top");
+      }
+    }
+
+    if (direction === "bottom") {
+      if (
+        adjacent[col + 1] !== undefined &&
+        matrix[col + 1][row] === 1 &&
+        adjacent[col + 1].indexOf(row) in adjacent[col] === false
+      ) {
+        adjacent[col + 1].push(row);
+        moveTopBot(col + 1, row, "bottom");
+      }
+    }
+  };
+
   // Start search from each border
-  const initializeSearch = () => isBorder(matrix);
+  const initializeSearch = () => isBorder();
   initializeSearch();
 
-  // Check for every [1] that's not in adjacent
-  // and convert it to [0]
-  // Skip every first and last number in matrix  
+  // Convert every [1] that's not in adjacent
+  // Skip every first and last number in matrix
   for (let col = 0; col < colLength; col++) {
     for (let row = 1; row < rowLength - 1; row++) {
       if (
@@ -193,11 +175,20 @@ const removeIslands = (matrix) => {
 
 // 5x5
 // let matrix = [
-//   [0, 1, 1, 1, 1],
-//   [1, 1, 0, 0, 0],
+  // [0, 1, 0, 1, 1],
+  // [0, 1, 1, 1, 0],
+  // [0, 0, 0, 0, 0],
+  // [0, 1, 1, 1, 0],
+  // [1, 1, 1, 1, 0],
+// ];
+
+// 5x5
+// let matrix = [
+//   [0, 1, 1, 1, 0],
+//   [1, 1, 0, 1, 0],
 //   [0, 1, 0, 1, 0],
-//   [0, 0, 0, 1, 0],
-//   [1, 0, 0, 1, 0],
+//   [0, 1, 1, 1, 0],
+//   [0, 1, 0, 1, 0],
 // ];
 
 // let matrix = [
@@ -209,15 +200,15 @@ const removeIslands = (matrix) => {
 // ]
 
 // 7x7
-let matrix = [
-  [1, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 0, 1, 1, 0],
-  [0, 1, 1, 1, 0, 0, 0],
-  [1, 0, 0, 1, 1, 1, 0],
-  [0, 1, 1, 0, 1, 0, 0],
-  [0, 1, 1, 0, 1, 1, 1],
-  [1, 0, 1, 1, 0, 0, 0],
-];
+// let matrix = [
+//   [1, 1, 0, 1, 0, 1, 0],
+//   [0, 1, 0, 0, 1, 1, 0],
+//   [0, 1, 1, 1, 0, 0, 0],
+//   [1, 0, 0, 1, 1, 1, 0],
+//   [0, 1, 1, 0, 1, 0, 0],
+//   [0, 1, 1, 0, 1, 1, 1],
+//   [1, 0, 1, 1, 0, 0, 0],
+// ];
 
 // 10x10
 // let matrix = [
@@ -232,6 +223,15 @@ let matrix = [
 //   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 //   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 // ];
+
+// 5x5
+let matrix = [
+  [0, 1, 1, 1, 0],
+  [1, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0],
+];
 
 // Original Matrix
 console.log(matrix);
